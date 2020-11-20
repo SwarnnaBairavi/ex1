1c80d04e75908cb6e75066edaa79bd68bf48eb9f
SwarnnaBairavi <74760936+SwarnnaBairavi@users.noreply.github.com>
2020-11-20 19:11:49 +0800

    Update login_proses.php

--- accomodation/login_proses.php
+++ accomodation/login_proses.php
@@ -2,34 +2,34 @@
 	session_start();
 	include 'config/koneksi.php';
 	
-	$user = @$_POST['username'];
+	$user = @$_POST['name'];
 	$pass = @$_POST['password'];
 	
-	$query=mysqli_query($conn,"select * from user where usernames='$user' and passwords='$pass'");
+	$query=mysqli_query($conn,"select * from user where names='$user' and passwords='$pass'");
 	$jumlahdata =mysqli_num_rows($query);
 	
 	if ($jumlahdata == 0 ) {
-		echo "<script>alert('username and password doesnt match !');
+		echo "<script>alert('name and password doesnt match !');
 				window.location.href='login.php';</script>";
 	} else{
 		$data= mysqli_fetch_array($query);
 
 		if($data['level']=="1"){
-			$_SESSION['username'] = $data['nama_user'];
+			$_SESSION['name'] = $data['nama_user'];
 			$_SESSION['level'] = "Manager";
 				echo "<script>alert('Login Success !');
 				</script>";
 			header("location:index.php");
 		
 		}else if($data['level']=="2"){
-			$_SESSION['username'] = $data['nama_user'];
+			$_SESSION['name'] = $data['nama_user'];
 			$_SESSION['level'] = "Student";
 			echo "<script>alert('Login Success !');
 				</script>";
 			header("location:index.php");
 		}
 		else if($data['level']=="3"){
-			$_SESSION['username'] = $data['nama_user'];
+			$_SESSION['name'] = $data['nama_user'];
 			$_SESSION['level'] = "Admin";
 				echo "<script>alert('Login Succes !');
 				</script>";
